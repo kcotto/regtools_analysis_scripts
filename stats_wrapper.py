a4ff088e69f58a69957dcb5f9f66caa6711cad4d
Kelsy Cotto <kelsy.cotto@wustl.edu>
2019-11-20 22:14:45 -0600

    update script

--- stats_wrapper.py
+++ stats_wrapper.py
@@ -7,6 +7,7 @@ cwd = os.getcwd()
 
 
 for tag in tags:
+    print(f'Working on {tag} tag')
     lines_per_file = 50000
     smallfile = None
     input_file = f'all_splicing_variants_{tag}.bed'
@@ -28,16 +29,16 @@ for tag in tags:
         print(f'Starting to run file chunk {count}')
         subprocess.run(f'Rscript --vanilla /home/ec2-user/workspace/data/compare_junctions_hist_v2.R {tag} {file}', shell=True, check=True)
         count += 1
-    if len(files) > 1:
-        subprocess.run('head -n1 small_file_50000.txt', shell=True, check=True)
-        subprocess.run('for fname in *.txt; do tail -n+2 $fname >> junctions_pvalues; done', shell=True, check=True)
         output_file = f'compare_junctions/hist/junctions_pvalues_{tag}.tsv'
-        if os.path.exists(output_file):
-            os.remove(output_file)
-        os.rename('junctions_pvalues.tsv', output_file)
-    else:
-        tmp_file = glob.glob('*out.tsv')[0]
-        if os.path.exists(output_file):
-            os.remove(output_file)
-        os.rename(tmp_file, output_file)
+        if len(files) > 1:
+            subprocess.run('head -n1 small_file_50000.txt_out.tsv', shell=True, check=True)
+            subprocess.run('for fname in *.txt; do tail -n+2 $fname >> junctions_pvalues; done', shell=True, check=True)
+            if os.path.exists(output_file):
+                os.remove(output_file)
+            os.rename('junctions_pvalues.tsv', output_file)
+        else:
+            tmp_file = glob.glob('*out.tsv')[0]
+            if os.path.exists(output_file):
+                os.remove(output_file)
+            os.rename(tmp_file, output_file)
     os.remove('small_file*')
