65695af7f7ed47876f9e72ed82fcaf5f029e9b23
EC2 Default User <ec2-user@ip-172-31-13-172.ec2.internal>
2020-01-02 18:40:46 +0000

    update shell script

--- run_stats_v2.sh
+++ run_stats_v2.sh
@@ -1,7 +1,7 @@
 
 tags=(E I default i50e5)
-# cohorts=(CHOL)
-cohorts=(DLBC UCS KICH MESO UVM ACC SKCM THYM GBM READ TGCT ESCA PAAD PCPG SARC OV KIRP CESC KIRC LIHC STAD BLCA COAD PRAD THCA LUSC HNSC LGG LUAD UCEC BRCA)
+cohorts=(CHOL)
+# cohorts=(DLBC UCS KICH MESO UVM ACC SKCM THYM GBM READ TGCT ESCA PAAD PCPG SARC OV KIRP CESC KIRC LIHC STAD BLCA COAD PRAD THCA LUSC HNSC LGG LUAD UCEC BRCA)
 
 for c in ${cohorts[@]}; do
 	mkdir -p ${c}/samples/
@@ -11,11 +11,11 @@ for c in ${cohorts[@]}; do
 	for t in *.gz; do
 		tar xzf ${t}
 		rm ${t}
-		rm -rf all*	
+		rm -rf all*
 		rm -rf compare_junctions
 	done
 	cd ..
-	ls samples/ > dir_names.tsv		
+	ls samples/ > dir_names.tsv
 	for k in ${tags[@]}; do
 		for i in samples/TCGA*; do
 			bash /data/variants.sh ${i}/cse_identify_filtered_${k}.tsv ${i}/variants_${k}.bed
@@ -26,24 +26,18 @@ for c in ${cohorts[@]}; do
 			uniq ${i}/variants_${k}.bed | awk '{split($0, a, ","); if (length(a[2]) != 0) print a[1]"\n"a[2]; else print a[1]}' | awk -v var=${j%%/} '{print $0 "\t" var}' >> all_splicing_variants_${k}.bed
 		done
 	done
-	Rscript --vanilla /home/ec2-user/regtools/scripts/compare_junctions_hist.R default
-	Rscript --vanilla /home/ec2-user/regtools/scripts/compare_junctions_hist.R i50e5
-	Rscript --vanilla /home/ec2-user/regtools/scripts/compare_junctions_hist.R E
-	Rscript --vanilla /home/ec2-user/regtools/scripts/compare_junctions_hist.R I
-	Rscript --vanilla /home/ec2-user/regtools/scripts/filter_and_BH.R default
-	Rscript --vanilla /home/ec2-user/regtools/scripts/filter_and_BH.R i50e5
-	Rscript --vanilla /home/ec2-user/regtools/scripts/filter_and_BH.R E
-	Rscript --vanilla /home/ec2-user/regtools/scripts/filter_and_BH.R I	
+	python3 /home/ec2-user/workspace/regtools/scripts/stats_wrapper.py default ${cohort}
+        python3 /home/ec2-user/workspace/regtools/scripts/stats_wrapper.py i50e5 ${cohort}
+        python3 /home/ec2-user/workspace/regtools/scripts/stats_wrapper.py E ${cohort}
+        python3 /home/ec2-user/workspace/regtools/scripts/stats_wrapper.py I ${cohort}
+	Rscript --vanilla /home/ec2-user/workspace/regtools/scripts/filter_and_BH.R default
+	Rscript --vanilla /home/ec2-user/workspace/regtools/scripts/filter_and_BH.R i50e5
+	Rscript --vanilla /home/ec2-user/workspace/regtools/scripts/filter_and_BH.R E
+	Rscript --vanilla /home/ec2-user/workspace/regtools/scripts/filter_and_BH.R I
 	for i in all_*; do
 		aws s3 cp ${i} s3://regtools-results-unstranded/${c}/
 	done
-	cd samples/
-	for i in *; do
-		tar -czf ${i}.tar.gz ${i}
-		aws s3 cp ${i}.tar.gz s3://regtools-results-unstranded/${c}/
-	done
-	cd ..
-	aws s3 cp compare_junctions/ s3://regtools-results-unstranded/${c}/compare_junctions/ --recursive
+	aws s3 cp compare_junctions/ s3://regtools-results-unstranded/${c}/compare_junctions2/ --recursive
 	cd ..
 	rm -rf ${c}*
 done
