8bff2bd1d0596c000970a5b2ba56f392bbb72e0d
Kelsy Cotto <kelsy.cotto@wustl.edu>
2020-01-05 18:29:11 -0600

    Update vep_aws_workflow.py

--- vep_aws_workflow.py
+++ vep_aws_workflow.py
@@ -112,7 +112,7 @@ stream_handler.setFormatter(log_formatter)
 stream_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 
-sqs = SQSWrapper('default')
+sqs = SQSWrapper('default', 'vep')
 
 try:
     logger.info('Waiting for work item')
