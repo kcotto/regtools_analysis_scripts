9acc1e9f364c3d59949384c312a911ad5a7d9369
EC2 Default User <ec2-user@ip-172-31-74-251.ec2.internal>
2020-06-23 04:17:43 +0000

    updates

--- combine_regtools_tsvs.py
+++ combine_regtools_tsvs.py
@@ -1,18 +1,53 @@
 import csv
+import subprocess
+import sys
+import os
 
-with open('/Users/kcotto/Downloads/junction_pvalues_default_old.tsv', 'r') as old, open('/Users/kcotto/Downloads/junction_pvalues_default_new.tsv', 'r') as new, open('/Users/kcotto/Downloads/junction_pvalues_default_merged.tsv', 'w') as outputfile:
-    old_reader = csv.reader(old, delimiter='\t')
-    new_reader = csv.reader(new, delimiter='\t')
-    keys = set()
-    header = next(new_reader)
-    header_line = '\t'.join(header)
-    outputfile.write(header_line + '\n')
-    next(old_reader)
-    for line in new_reader:
-        keys.add(line[9])
-        joined_line = '\t'.join(line)
-        outputfile.write(joined_line + '\n')
-    for line in old_reader:
-        if line[9] not in keys:
-            joined_line = '\t'.join(line)
-            outputfile.write(joined_line + '\n')
+def run(cmd):
+    subprocess.run(cmd, shell=True, check=True, stdout=sys.stdout)
+
+# with open('/home/ec2-user/old_files.txt', 'r') as old, open('/home/ec2-user/fixed_files.txt', 'r') as new:
+#     old_files = old.readlines()
+#     new_files = new.readlines()
+#     old_file_keys = set()
+#     new_file_keys = set()
+#     for file in old_files:
+#         fields = file.strip().split('/')
+#         key = f'{fields[0]}|{fields[-1]}'
+#         old_file_keys.add(key)
+#     for file in new_files:
+#         fields = file.strip().split('/')
+#         key = f'{fields[0]}|{fields[-1]}'
+#         new_file_keys.add(key)
+    # keys_to_copy = old_file_keys - new_file_keys
+    # for key in keys_to_copy:
+    #     fields = key.split('|')
+    #     run(f'aws s3 cp s3://regtools-results-unstranded/{fields[0]}/compare_junctions2/hist/{fields[1]} s3://regtools-results-unstranded/{fields[0]}/compare_junctions/hist/{fields[1]}')
+
+
+with open('/home/ec2-user/fixed_files.txt', 'r') as new:
+    new_files = new.readlines()
+    for new_file in new_files:
+        fields = new_file.strip().split('/')
+        run(f'aws s3 cp s3://regtools-results-unstranded/{fields[0]}/compare_junctions3/hist/{fields[-1]} new_file.tsv')
+        run(f'aws s3 cp s3://regtools-results-unstranded/{fields[0]}/compare_junctions2/hist/{fields[-1]} old_file.tsv')
+        with open('old_file.tsv', 'r') as old, open('new_file.tsv', 'r') as new, open('merged.tsv', 'w') as outputfile:
+            old_reader = csv.reader(old, delimiter='\t')
+            new_reader = csv.reader(new, delimiter='\t')
+            keys = set()
+            header = next(new_reader)
+            header_line = '\t'.join(header)
+            outputfile.write(header_line + '\n')
+            next(old_reader)
+            for line in new_reader:
+                keys.add(line[9])
+                joined_line = '\t'.join(line)
+                outputfile.write(joined_line + '\n')
+            for line in old_reader:
+                if line[9] not in keys:
+                    joined_line = '\t'.join(line)
+                    outputfile.write(joined_line + '\n')
+            run(f'aws s3 cp merged.tsv s3://regtools-results-unstranded/{fields[0]}/compare_junctions/hist/{fields[-1]}')
+        os.remove('old_file.tsv')
+        os.remove('new_file.tsv')
+        os.remove('merged.tsv')
